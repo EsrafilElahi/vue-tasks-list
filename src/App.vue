e97163ed476d528907928e27df68e88328c120cb
esrafil elahi <esrafil.elahi@gmail.com>
2024-05-05 15:07:16 +0330

    intall vue-router

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <div class="item">
         <h4>title</h4>
         <div class="actions">
-          <span>delete</span>
+          <span class="material-icons tick">delete</span>
           <span>edit</span>
           <span>complete</span>
         </div>
@@ -39,11 +39,21 @@ html body {
 }
 
 .container {
-  padding: .8rem .5rem;
-  border: 1px solid red;
-  border-radius: .3rem;
+  width: 100%;
+  height: 100%;
+  padding: 5rem 0;
+}
+
+.box-items {
   max-width: 25rem;
   width: 100%;
+  margin-inline: auto;
+  padding: 1.3rem .8rem;
+  /* border: 1px solid red; */
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.5);
+  border-left: 10px solid red;
+  box-shadow: 1rem;
+  border-radius: .3rem;
 }
 
 .item {
