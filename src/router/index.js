6deb5571c520039f3a7e3dde11ec48297d6fea9d
esrafil elahi <esrafil.elahi@gmail.com>
2024-05-05 15:45:41 +0330

    add routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,19 @@
 import { createMemoryHistory, createRouter } from "vue-router";
+
+const routes = [
+  {
+    path: "/",
+    component: import("../views/HomeView.vue"),
+  },
+  {
+    path: "/add",
+    component: import("../views/AddTaskView.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+});
+
+export default router;
